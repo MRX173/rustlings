82357017a5127233de7e014a8e10972a3a4adff5
MRX173 <ms8685500@gmail.com>
2024-04-04 17:45:44 +0200

    Solve enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,9 +5,12 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
+    ChangeColor(i32,i32,i32),
+    Quit(bool),
+    Echo(String),
+    Move(Point)
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -54,15 +57,15 @@ mod tests {
     #[test]
     fn test_match_message_call() {
         let mut state = State {
-            quit: false,
-            position: Point { x: 0, y: 0 },
-            color: (0, 0, 0),
-            message: "hello world".to_string(),
+            quit: true,
+            position: Point { x: 10, y: 15 },
+            color: (255, 0, 255),
+            message: String::from("Hello world!"),
         };
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("Hello world!")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
-        state.process(Message::Quit);
+        state.process(Message::Quit(true));
 
         assert_eq!(state.color, (255, 0, 255));
         assert_eq!(state.position.x, 10);
